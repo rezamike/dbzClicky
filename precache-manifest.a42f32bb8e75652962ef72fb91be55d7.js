cb1fba4176280656eed3abff28bf91476487552e
rezamike <michael.sanaiha@gmail.com>
2018-10-23 17:24:41 -0700

    Updates

---
+++ precache-manifest.a42f32bb8e75652962ef72fb91be55d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "805a7a917f6f5fd07f04",
+    "url": "/static/js/main.805a7a91.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "805a7a917f6f5fd07f04",
+    "url": "/static/css/main.2b8bbc20.chunk.css"
+  },
+  {
+    "revision": "f4ef816ae0b7a6af2884c6ec1c9f7ab9",
+    "url": "/index.html"
+  }
+];
